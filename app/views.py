2a492891cdb13a390f6150dd523ef434aaa4b2eb
Tiffany Schoneboom <t.sboom@gmail.com>
2016-03-09 16:46:41 -0500

    remove some unnecessary files

--- app/views.py
+++ app/views.py
@@ -1,20 +1,21 @@
-from app import app, render_template, py
-import highlightsnew.py
+from app import app, render_template
+
+import highlightsnew
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/submit/')
+@app.route('/submit')
 def submit():
     print "hello world!"
     
-    doiList = request.form["pasteddois"]
-    with doiLIST as infile:
-        myDOIs = [line.strip() for line in infile]
-    # run python process
-    result = processDOI(myDOIs)
+    # doiList = request.form["pasteddois"]
+    # with doiLIST as infile:
+    #     myDOIs = [line.strip() for line in infile]
+    # # run python process
+    # result = processDOI(myDOIs)
 
 
 
